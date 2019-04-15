8b169294546f38d62ce5eae47eea4abae0210610
David Larsson <david.larsson89@gmail.com>
2019-04-15 12:42:43 +0200

    Added model data

--- src/main/java/se/dala/mtgcollectionapi/model/CardInfo.java
+++ src/main/java/se/dala/mtgcollectionapi/model/CardInfo.java
@@ -1,4 +1,38 @@
 package se.dala.mtgcollectionapi.model;
 
 public class CardInfo {
+    private String name;
+    private String imageUrl;
+    private String number;
+
+    public CardInfo() {}
+    public CardInfo(String name, String imageUrl, String number) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
--- src/main/java/se/dala/mtgcollectionapi/model/MtgSetEdit.java
+++ src/main/java/se/dala/mtgcollectionapi/model/MtgSetEdit.java
@@ -1,4 +1,21 @@
 package se.dala.mtgcollectionapi.model;
 
+import java.util.List;
+
 public class MtgSetEdit {
+    private String name;
+    private List<CardInfo> cards;
+
+    public MtgSetEdit(String name, List<CardInfo> cards) {
+        this.name = name;
+        this.cards = cards;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<CardInfo> getCards() {
+        return cards;
+    }
 }
